a29ae105c2f91f5d1c0890e696cb8801b2091539
Enrique Ponce De Leon <enrique@processmaker.com>
2022-06-13 09:50:13 -0400

    adding fetch.sh script

--- build.sh
+++ build.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 
-# Store current path
-SCRIPT_PATH=$(pwd)
-
 # Retrieve version
 TAG=$(cat composer.json | jq -r '.version')
-
-# Updating git repository
-if [ ! -d processmaker/.git ]; then
-    rm -rf processmaker
-    git clone https://github.com/ProcessMaker/processmaker.git
-else
-    cd $SCRIPT_PATH/processmaker && git fetch --all
-fi
-
-# Checkout version
-cd $SCRIPT_PATH/processmaker && git checkout $(cat $SCRIPT_PATH/version)
 
 # Build image
 cd $SCRIPT_PATH && docker build -f Dockerfile.sdk -t processmaker/docker-executor-php:$TAG .
